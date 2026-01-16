bcbb0aa24558beeb5ec3ac489379f28d43286d8f
Mark Baiden <markbaiden17@gmail.com>
2026-01-16 14:49:40 +0000

    edit views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from urllib3 import request
-from .models import Book, Library
+from .models import Book 
+from .models import Library
 
 # Create your views here.
 #Function-based view to display all books
