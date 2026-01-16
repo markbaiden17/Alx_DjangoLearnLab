1d95a7b576586cdd5b22d3b5df4a3589c058b479
Mark Baiden <markbaiden17@gmail.com>
2026-01-16 16:13:05 +0000

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -27,4 +27,8 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     # Registration using your custom view
     path('register/', views.register, name='register'),
+
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
@@ -37,3 +38,26 @@ def register(request):
 def logout_user(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
+# Helper functions to check roles
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+# Role-based views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
