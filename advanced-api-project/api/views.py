92bdd47aab0372348be89bbdc44429d0f3e08ab9
Mark Baiden <markbaiden17@gmail.com>
2026-02-05 09:36:16 +0000

    Edit api/views.py: add new import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django_filters import rest_framework
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
