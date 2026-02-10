0e201b8a4ee8a0f6a7faec8bd893482f54385206
Mark Baiden <markbaiden17@gmail.com>
2026-02-10 10:09:32 +0000

    Edit views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
